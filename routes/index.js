33ee6de315b8cdadcb7daab7699d0879db59c340
cristina <cristinaaran89@gmail.com>
2022-06-27 15:35:02 +0200

    peli finish

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movie= require('../models/Movie.model.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', async (req, res, next) => {
+    try {
+        const pelis = await Movie.find({});
+        console.log('estas son las peliculas', pelis)
+        res.render('movies', {pelis:pelis})
+    }catch (err) {
+        console.log('error', err)
+    }
+});
+
+router.get('/movies/:id',async (req, res, next) => { 
+    try {
+        console.log('QUIERO ESTE PUTO ID',req.params.id)
+        const peli =await Movie.findById(req.params.id)
+        res.render('movieOne', { peli: peli })
+        
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 
 module.exports = router;
